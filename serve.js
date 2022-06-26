47e4e60e67a7b613da7986200facc93190944046
Vamberto Silva <athelstanmorchiori@gmail.com>
2022-06-26 12:06:46 -0300

    corrigindo bug do public

--- serve.js
+++ serve.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const port = process.env.PORT || 8000;
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.json({
-    msg: "OK",
-  });
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/op", (req, res) => {

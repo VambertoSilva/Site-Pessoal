dd366815e181d8f16e843e757c7f34e356474b52
Vamberto Silva <athelstanmorchiori@gmail.com>
2022-07-01 10:48:24 -0300

    Segunda viewport

--- serve.js
+++ serve.js
@@ -9,10 +9,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.get("/op", (req, res) => {
-  res.json({
-    msg: "funcionando",
-  });
+app.get("/construcao", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 app.listen(port, () => {
   console.log(`usando a porta ${port}`);

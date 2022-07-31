03723932116b3ac7fa70044c43e57c57efd896ca
Vamberto Silva <athelstanmorchiori@gmail.com>
2022-07-31 19:15:36 -0300

    Finalizado

---
+++ public/app.js
@@ -0,0 +1,16 @@
+const menu = document.querySelectorAll(".menu h4[href^='#']");
+
+menu.forEach(item => {
+  item.addEventListener("click", () => {
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute("href");
+    const section = document.querySelector(id);
+    const to = section.offsetTop;
+
+    window.scroll({
+      top: to,
+      behavior: "smooth",
+    });
+  });
+});
